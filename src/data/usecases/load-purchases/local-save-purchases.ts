3e9f9cd1b80500f6b21129c0add5e1353ad6f55f
James Leal <jamesleal@Jamess-MacBook-Air.local>
2022-01-17 18:22:12 -0300

    rename class

--- src/data/usecases/load-purchases/local-save-purchases.ts
+++ src/data/usecases/load-purchases/local-save-purchases.ts
@@ -1,7 +1,7 @@
 import { CacheStore } from "@/data/protocols/cache/cache-store";
 import { SavePurchases } from "@/domain/usecases";
 
-export class LocalSavePurchases implements SavePurchases {
+export class LocalLoadPurchases implements SavePurchases {
   constructor(
     private readonly cacheStore: CacheStore,
     private readonly timestamp: Date
