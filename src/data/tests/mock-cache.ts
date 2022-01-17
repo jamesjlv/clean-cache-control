6e88ae3b40c2debb5020f729c70a8912f9ba611b
James Leal <jamesleal@Jamess-MacBook-Air.local>
2022-01-17 17:33:25 -0300

    simplify CacheStore insertion with a replace method

--- src/data/tests/mock-cache.ts
+++ src/data/tests/mock-cache.ts
@@ -19,6 +19,11 @@ export class CacheStoreSpy implements CacheStore {
     this.insertValues = values;
   }
 
+  replace(key: string, value: any): void {
+    this.delete(key);
+    this.insert(key, value);
+  }
+
   simulateDeleteError(): void {
     jest.spyOn(CacheStoreSpy.prototype, "delete").mockImplementationOnce(() => {
       this.actions.push(CacheStoreSpy.Action.delete);
