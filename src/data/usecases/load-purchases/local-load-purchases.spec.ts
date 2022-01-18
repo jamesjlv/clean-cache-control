c87a26c6134ab1bf4540f1b457918bbe19b6ec93
James Leal <jamesleal@Jamess-MacBook-Air.local>
2022-01-18 00:56:17 -0300

    ensure LocalLoadPurchases has no effects on load with invalid cache age

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -55,12 +55,8 @@ describe("LocalLoadPurchases", () => {
       value: mockPurchases(),
     };
     const purchases = await sut.loadAll();
-    expect(cacheStore.actions).toEqual([
-      CacheStoreSpy.Action.fetch,
-      CacheStoreSpy.Action.delete,
-    ]);
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
     expect(cacheStore.fetchKey).toBe("purchases");
-    expect(cacheStore.deleteKey).toBe("purchases");
     expect(purchases).toEqual([]);
   });
 
@@ -73,12 +69,8 @@ describe("LocalLoadPurchases", () => {
       value: mockPurchases(),
     };
     const purchases = await sut.loadAll();
-    expect(cacheStore.actions).toEqual([
-      CacheStoreSpy.Action.fetch,
-      CacheStoreSpy.Action.delete,
-    ]);
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
     expect(cacheStore.fetchKey).toBe("purchases");
-    expect(cacheStore.deleteKey).toBe("purchases");
     expect(purchases).toEqual([]);
   });
 
