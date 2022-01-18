c96ad41f6ccd1b74db2bf2ce1d08334b2bf803c9
James Leal <jamesleal@Jamess-MacBook-Air.local>
2022-01-18 00:26:37 -0300

    extract cache max age in a variable

--- src/data/tests/mock-cache.ts
+++ src/data/tests/mock-cache.ts
@@ -1,9 +1,11 @@
-import { LoadPurchases, SavePurchases } from "@/domain/usecases";
+import { SavePurchases } from "@/domain/usecases";
 import { CacheStore } from "../protocols/cache";
+
+const maxAgeInDays = 3;
 
 export const getCacheExpirationDate = (timestamp: Date): Date => {
   const maxCacheAge = new Date(timestamp);
-  maxCacheAge.setDate(timestamp.getDate() - 3);
+  maxCacheAge.setDate(timestamp.getDate() - maxAgeInDays);
   return maxCacheAge;
 };
 
