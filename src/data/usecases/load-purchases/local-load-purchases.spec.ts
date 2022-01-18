3a84d67c6577180ccf2baee848aa80f000ea5d6e
James Leal <jamesleal@Jamess-MacBook-Air.local>
2022-01-18 00:48:06 -0300

    ensure localloadpurchases has no side effects on load if it fails

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -22,22 +22,11 @@ describe("LocalLoadPurchases", () => {
     expect(cacheStore.actions).toEqual([]);
   });
 
-  // test("Should call correct key on load", async () => {
-  //   const { cacheStore, sut } = makeSut();
-  //   await sut.loadAll();
-  //   expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
-  //   expect(cacheStore.fetchKey).toBe("purchases");
-  // });
-
   test("Should return empty list if load fails", async () => {
     const { cacheStore, sut } = makeSut();
     cacheStore.simulateFetchError();
     const purchases = await sut.loadAll();
-    expect(cacheStore.actions).toEqual([
-      CacheStoreSpy.Action.fetch,
-      CacheStoreSpy.Action.delete,
-    ]);
-    expect(cacheStore.deleteKey).toBe("purchases");
+    expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
     expect(purchases).toEqual([]);
   });
 
